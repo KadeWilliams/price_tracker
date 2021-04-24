2040a550a5cbedb504104141fbb52ee31b310a1a
kadew <kadewilliams96@gmail.com>
2021-04-24 14:59:07 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+sending_email = 'a.python.smtp.test@gmail.com'
+sending_password = 'QJcppZ4y69npg7D'
+receiving_email = 'kadewilliams0@gmail.com'
+
+import requests
+from bs4 import BeautifulSoup
+import smtplib
+
+PRODUCT_PAGE = 'https://www.amazon.com/ChefSteps-Joule-Watts-White-Stainless/dp/B01M8MMLBI/ref=sr_1_4?dchild=1&keywords=joule&qid=1619292897&sr=8-4'
+alert_price = 200.0
+head = {
+    'Accept-Language':'en-US,en;q=0.9,es-US;q=0.8,es;q=0.7,ko-KR;q=0.6,ko;q=0.5',
+    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.128 Safari/537.36'
+}
+
+response = requests.get(PRODUCT_PAGE,
+                        headers=head)
+html = response.text
+soup = BeautifulSoup(html, 'html.parser')
+price = float(soup.find('span', id='price_inside_buybox').getText().split('$')[1])
+
+if price < alert_price:
+    with smtplib.SMTP('smtp.gmail.com', port=587) as connection:
+        print('sending...')
+        connection.starttls()
+        connection.login(user=sending_email, password=sending_password)
+        connection.sendmail(
+            from_addr=sending_email,
+            to_addrs=receiving_email,
+            msg=f'Subject: Price Alert\n\nJoule is ${price}, ${alert_price-price} lower than your alert value!')
+
